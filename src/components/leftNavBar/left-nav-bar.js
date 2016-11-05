cbb97bc04c7b0164a98fd9edc4f42f449401de14
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-11-06 00:40:28 +0800

    update

--- src/components/leftNavBar/left-nav-bar.js
+++ src/components/leftNavBar/left-nav-bar.js
@@ -7,19 +7,42 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 export class LeftNavBar {
   @bindable({ defaultBindingMode: bindingMode.twoWay }) currentNav;
 
-  toggleSecondNav = (_index, _event) => {
-    this.navigations[_index].firstNavOpen = !this.navigations[_index].firstNavOpen;
-    (this.navigations[_index].firstNavOpen)
-      ?$(_event.target).parents('li.first-nav').find('ul').slideDown(300)
-      :$(_event.target).parents('li.first-nav').find('ul').slideUp(300);
+  timeoutID = null;
+
+  toggleSecondNav = (_firstNav, _event) => {
+    _firstNav.firstNavOpen = !_firstNav.firstNavOpen;
+    (_firstNav.firstNavOpen)
+      ?$(_event.target).parents('li.first-nav').find('ul.second-ul').slideDown(300)
+      :$(_event.target).parents('li.first-nav').find('ul.second-ul').slideUp(300);
   };
+
+  toggleThirdNav = (_secondNav, _event, _open) => {
+    if(_open){
+      _secondNav.secondNavOpen = _open;
+      $(_event.target).parents('li.second-nav').find('ul.third-ul').slideDown(300);
+    }
+    else{
+      this.timeoutID = setTimeout(() => {
+        if(!_secondNav.secondNavBlock){
+          _secondNav.secondNavOpen = _open;
+          $(_event.target).parents('li.second-nav').find('ul.third-ul').slideUp(300);
+        }
+      }, 200);
+    }
+  };
+
+  setNavBlock(_secondNav, _event, _block) {
+    _secondNav.secondNavBlock = _block;
+    if(!_secondNav.secondNavBlock){
+      $(_event.target).slideUp(300);
+    }
+  }
 
   navigations = [{
     "firstNavName": "首页",
     "firstNavIcon": "icon-menu",
     "firstNavOpen": false,
-    "firstNavCallback": () => {},
-    "secondNavs": []
+    "firstNavCallback": () => {}
   },{
     "firstNavName": "产品与服务",
     "firstNavIcon": "icon-arrow",
@@ -29,38 +52,54 @@ export class LeftNavBar {
       "secondNavName": "TDH服务",
       "secondNavIcon": "icon-settings",
       "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
+      "secondNavCallback": () => {}
     },{
       "secondNavName": "Transwarp Studio",
       "secondNavIcon": "icon-settings",
       "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
-    },{
-      "secondNavName": "数据资产",
-      "secondNavIcon": "icon-settings",
-      "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
-    },{
-      "secondNavName": "项目资产",
-      "secondNavIcon": "icon-settings",
-      "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
-    },{
-      "secondNavName": "应用市场",
-      "secondNavIcon": "icon-settings",
-      "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
-    },{
-      "secondNavName": "模型商店",
-      "secondNavIcon": "icon-settings",
-      "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
+      "secondNavBlock": false,//if open is blocked
+      "secondNavCallback": this.toggleThirdNav,
+      "thirdNavs": [{
+        "thirdNavName": "报表",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "Notebook",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "分析",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "工作流",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "OLAP",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "元数据",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "TDT",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "BulkLoad",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "HDFS浏览",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      },{
+        "thirdNavName": "Transpedia",
+        "thirdNavIcon": "icon-settings",
+        "thirdNavCallback": () => {}
+      }]
     }]
   },{
     "firstNavName": "管理",
@@ -71,20 +110,17 @@ export class LeftNavBar {
       "secondNavName": "租户",
       "secondNavIcon": "icon-settings",
       "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
+      "secondNavCallback": () => {}
     },{
       "secondNavName": "用户",
       "secondNavIcon": "icon-settings",
       "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
+      "secondNavCallback": () => {}
     },{
       "secondNavName": "角色",
       "secondNavIcon": "icon-settings",
       "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
+      "secondNavCallback": () => {}
     }]
   },{
     "firstNavName": "个人",
@@ -95,14 +131,12 @@ export class LeftNavBar {
       "secondNavName": "消息",
       "secondNavIcon": "icon-settings",
       "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
+      "secondNavCallback": () => {}
     },{
       "secondNavName": "工单",
       "secondNavIcon": "icon-settings",
       "secondNavOpen": false,
-      "secondNavCallback": () => {},
-      "thirdNavs": []
+      "secondNavCallback": () => {}
     }]
   }];
 
@@ -113,6 +147,12 @@ export class LeftNavBar {
   
   attached() {
     console.log(this.currentNav);
+  }
+
+  detached(){
+    if(this.timeoutID){
+      clearTimeout(this.timeoutID)
+    }
   }
   
   navigate(name) {
