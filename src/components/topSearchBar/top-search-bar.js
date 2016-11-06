698d253aacaa5b6662c4536330b91cdf5ffbfc4e
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-11-06 23:28:43 +0800

    update

---
+++ src/components/topSearchBar/top-search-bar.js
@@ -0,0 +1,36 @@
+import {inject, bindable, bindingMode, containerless} from 'aurelia-framework';
+import {Router} from "aurelia-router";
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(Router, EventAggregator)
+@containerless()
+export class TopSearchBar {
+
+  username = "support@transwarp.io";
+  showUserInfo = false;
+
+  toggleMenu() {
+    this.showUserInfo = !this.showUserInfo;
+  }
+  
+  closeMenu() {
+    this.showUserInfo = false;
+  }
+
+  constructor(router, ea) {
+    this.router = router;
+    this.eventAggregator = ea;
+  }
+
+  attached() {
+
+  }
+  
+  setup() {
+    console.log("setup");
+  }
+
+  logout() {
+    console.log("logout");
+  }
+}
