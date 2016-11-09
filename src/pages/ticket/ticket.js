3d5133ba788aec2c6d62e46730d712ba7e20a6cb
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-11-09 17:43:30 +0800

    fix bug

--- src/pages/ticket/ticket.js
+++ src/pages/ticket/ticket.js
@@ -1,14 +1,38 @@
 import {inject} from "aurelia-framework";
 import {Router} from "aurelia-router";
+import {$utils} from "../../services/utils";
 
-@inject(Router)
+@inject(Router, $utils)
 export class Ticket {
+
+  query = {
+    "ticketNo": "",
+    "startDate": "",
+    "endDate": "",
+    "keyword": ""
+  };
   
-  constructor(r) {
+  constructor(r, u) {
     this.router = r;
+    this.$utils = u;
   }
   
   attached() {
+    
+  }
+
+  cleanQuery(_targetKey, keys) {
+    if(this.$utils.trim(this.query[_targetKey]) === "") return;
+    keys.forEach((key) => {
+      this.query[key] = "";
+    });
+  }
+
+  createTicket() {
+    
+  }
+  
+  search() {
     
   }
 }
